efa8979fb171faa5c50472e8d6733c45b4d9b2a2
sridhara-omics <sridhara.omics@gmail.com>
2023-02-06 11:07:40 +0530

    improve generic analysis R script

--- R/generic_seurat_analysis.R
+++ R/generic_seurat_analysis.R
@@ -10,39 +10,30 @@ library(Seurat)
 #' @examples
 generic_seurat_analysis <- function(data, scale.factor=1000, nfeatures=20, num.replicate=100, dims = 1:10, resolution=0.6, min.pct = 0.25, logfc.threshold = 0.25) {
 
-  # Setup the Seurat Object
+  # Create Seurat object
   sce <- CreateSeuratObject(counts = data, project = "Seurat_Analysis")
 
-  # Standard pre-processing workflow
+  # Pre-processing
   sce <- NormalizeData(sce, normalization.method = "LogNormalize", scale.factor = scale.factor)
   sce <- FindVariableFeatures(sce, selection.method = "vst", nfeatures = nfeatures)
-
-  # Normalizing the data
   sce <- ScaleData(sce)
 
-  # Identification of highly variable features (feature selection)
+  # PCA and feature selection
   sce <- RunPCA(sce, features = VariableFeatures(sce), pcs.print = 1:10)
-
-  # Scaling the data
   sce <- JackStraw(sce, num.replicate = num.replicate)
-  # sce <- ScoreJackStraw(sce)
 
-  # Perform linear dimensional reduction
+  # Dimensionality reduction
   sce <- FindNeighbors(sce, reduction = "pca", dims = dims)
   sce <- FindClusters(sce, resolution = resolution)
-
-  # Determine the ‘dimensionality’ of the dataset
   sce@meta.data$dimension <- as.factor(unique(sce@meta.data$cluster))
-
-  # Cluster the cells
   sce <- RunUMAP(sce, dims = dims)
 
-  # Run non-linear dimensional reduction (UMAP/tSNE)
+  # Finding markers and scoring cell types
   sce <- FindAllMarkers(sce, only.pos = TRUE, min.pct = min.pct, logfc.threshold = logfc.threshold)
-
-  # Finding differentially expressed features (cluster biomarkers)
   sce <- CellTypeScore(sce, dims = c(1,2), reduction = "umap")
 
-  # Assigning cell type identity to clusters
   return(sce)
 }
+
+
+
